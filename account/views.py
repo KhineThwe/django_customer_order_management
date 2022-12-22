0b14a5e3a8fe46c436a15c74774866532a89e37f
Khine <khinezar70122107@gmail.com>
2022-12-21 16:58:39 -1200

    dashboard show

--- account/views.py
+++ account/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from . models import *
 
 def home(request):
-    return render(request,"account/dashboard.html")
+    order = Order.objects.all()
+    customer = Customer.objects.all()
+    total_customers = customer.count()
+    total_orders = order.count()
+    delivered = order.filter(status='Delivered').count()
+    pending = order.filter(status='Pending').count()
+    
+    context = {'order':order,'customer':customer,'total_customers':total_customers
+               ,'total_orders':total_orders,'delivered':delivered,'pending':pending}
+    return render(request,"account/dashboard.html",context)
 
 def customer(request):
     return render(request,"account/customer.html")
 
 def products(request):
-    return render(request,"account/products.html")
+    products = Product.objects.all()
+    return render(request,"account/products.html",{'products':products})
